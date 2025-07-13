1445368eca356220d22384f6fc6f51df72a9e3ec
Ekalabya Ghosh <154486133+not-ekalabya@users.noreply.github.com>
2025-07-13 13:17:05 +0530

    Add files via upload

---
+++ violence.py
@@ -0,0 +1,26 @@
+import torch
+from transformers import ViTForImageClassification, ViTFeatureExtractor
+from PIL import Image
+
+import warnings
+warnings.filterwarnings("ignore")
+
+# Load the model and feature extractor
+model = ViTForImageClassification.from_pretrained('jaranohaal/vit-base-violence-detection')
+feature_extractor = ViTFeatureExtractor.from_pretrained('jaranohaal/vit-base-violence-detection')
+
+# Load an image
+image = Image.open('test.jpg')
+
+# Preprocess the image
+inputs = feature_extractor(images=image, return_tensors="pt")
+
+# Perform inference
+with torch.no_grad():
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class_idx = logits.argmax(-1).item()
+
+# Print the predicted class
+prediction = model.config.id2label[predicted_class_idx]
+print(f"Predicted class: {prediction}")
